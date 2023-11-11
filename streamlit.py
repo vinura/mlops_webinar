da0b46a346f63f834e27cf3b167b983ea37d662a
Vinura Perera <vinurap4@gmail.com>
2023-11-11 10:35:23 +0530

    Streamlit App Updated

--- streamlit.py
+++ streamlit.py
@@ -4,15 +4,19 @@ from torchvision import transforms
 from PIL import Image
 import mlflow
 
+# Streamlit app
+st.title("Cat vs. Dog Classifier")
+
+tracking_url = st.sidebar.text_input("Enter Tracking URI")
+current_stage = st.sidebar.selectbox("Stage", ("None","Staging", "Production", "Archived"))
+uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "png", "jpeg"])
+
 # Initialize MLflow
-mlflow.set_tracking_uri("https://991c-35-231-224-155.ngrok-free.app/")  # Set the path to your MLflow tracking server
+mlflow.set_tracking_uri(tracking_url)  # Set the path to your MLflow tracking server
 
 # Load the PyTorch model from MLflow
 MODEL_NAME = 'mercon'
-current_stage = 'Production'
 model_uri=f"models:/{MODEL_NAME}/{current_stage}"
-
-model = mlflow.pytorch.load_model(model_uri)
 
 # Define data transformations for inference
 data_transform = transforms.Compose([
@@ -24,17 +28,13 @@ data_transform = transforms.Compose([
 # Define class labels
 class_labels = ['Cat', 'Dog']
 
-# Streamlit app
-st.title("Cat vs. Dog Classifier")
-
-current_stage = st.sidebar.selectbox("Stage", ("None","Staging", "Production", "Archived"))
-uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "png", "jpeg"])
-
 if uploaded_image is not None:
     st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
     # Make a prediction when a user uploads an image
     if st.button("Classify"):
+        model = mlflow.pytorch.load_model(model_uri)
+
         image = Image.open(uploaded_image)
         image = data_transform(image).unsqueeze(0)  # Preprocess the image
 
